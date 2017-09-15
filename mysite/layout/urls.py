312105a48632bf8786ac20caef6fdece9c954c88
Khanh <vo.hongkhanh1010@gmail.com>
2017-09-16 01:03:23 +0700

    adding

--- mysite/layout/urls.py
+++ mysite/layout/urls.py
@@ -6,5 +6,13 @@ urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
     url(r'^about/$', views.AboutPageView.as_view()),
     url(r'^contact/$', views.ContactPageView.as_view()),
-    url(r'^courses/$', views.CoursePageView.as_view()), # Add this /about/ route
+    url(r'^courses/$', views.CoursePageView.as_view()),
+    url(r'^tin-tuc-su-kien/$', views.TinTucView.as_view()),
+    url(r'^ielts/$', views.IELTSPageView.as_view()),
+    url(r'^toeic/$', views.TOEICPageView.as_view()), # Add this /about/ route
+    url(r'^vstep/$', views.VSTEPPageView.as_view()),
+    url(r'^giao-tiep/$', views.GiaoTiepPageView.as_view()),
+    url(r'^thanh-tich-ielts/$', views.ThanhTichIeltsPageView.as_view()),
+    url(r'^thanh-tich-toeic/$', views.ThanhTichToeicPageView.as_view()),
+    url(r'^thanh-tich-vstep/$', views.ThanhTichVstepPageView.as_view()),
 ]
--- mysite/layout/views.py
+++ mysite/layout/views.py
@@ -11,5 +11,29 @@ class AboutPageView(TemplateView):
 class CoursePageView(TemplateView):
     template_name = "courses.html"
 
+class TinTucView(TemplateView):
+    template_name = "tin-tuc-su-kien.html"
+
 class ContactPageView(TemplateView):
     template_name = "contact.html"
+
+class IELTSPageView(TemplateView):
+    template_name = "ielts.html"
+
+class TOEICPageView(TemplateView):
+    template_name = "toeic.html"
+
+class VSTEPPageView(TemplateView):
+    template_name = "vstep.html"
+
+class GiaoTiepPageView(TemplateView):
+    template_name = "giao-tiep.html"
+
+class ThanhTichIeltsPageView(TemplateView):
+    template_name = "thanh-tich-ielts.html"
+
+class ThanhTichVstepPageView(TemplateView):
+    template_name = "thanh-tich-vstep.html"
+
+class ThanhTichToeicPageView(TemplateView):
+    template_name = "thanh-tich-toeic.html"
